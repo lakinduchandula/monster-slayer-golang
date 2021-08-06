5a4416691e6f7681586fcf0e71c15af131f4b7bb
lakinduchandula <lakindu.chandula@hotmail.com>
2021-08-06 18:36:58 +0530

    implement the logic player, monster attack and heal

--- action/action.go
+++ action/action.go
@@ -9,11 +9,51 @@ import (
 var randomSource = rand.NewSource(time.Now().UnixNano())
 var randomGenerator = rand.New(randomSource)
 
-func AttackMonster() {}
+// player-monster health values
+var currentPlayerHealth = 100
+var currentMonsterHealth = 100
 
-func HealPlayer() {}
+func AttackMonster(isSpecialAttack bool) {
+	// min-max attack value for general attack
+	minAttackValue := 5
+	maxAttackValue := 10
 
-func AttackPlayer() {}
+	if isSpecialAttack {
+		minAttackValue = 10
+		maxAttackValue = 20
+	}
+
+	// calculate damage-value
+	damageValue := generateRandomBetween(minAttackValue, maxAttackValue)
+	currentMonsterHealth -= damageValue // deduct damageValue from currentMonsterHealth
+
+}
+
+func HealPlayer() {
+	// heal values for general case
+	minHealValue := 7
+	maxHealValue := 15
+
+	// calculate heal-value
+	healValue := generateRandomBetween(minHealValue, maxHealValue)
+	currentPlayerHealth += healValue // current heal value
+
+	// system won't allow playerHealth to be greater than 100
+	if currentPlayerHealth > 100 {
+		currentPlayerHealth = 100
+	}
+}
+
+func AttackPlayer() {
+	minAttackValue := 8
+	maxAttackValue := 15
+
+	// calculate attack-player value
+	playerAttackValue := generateRandomBetween(minAttackValue, maxAttackValue)
+
+	// update current-player-health
+	currentPlayerHealth -= playerAttackValue
+}
 
 func generateRandomBetween(min int, max int) int {
 	return randomGenerator.Intn(max-min) + min
